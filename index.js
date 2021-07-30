5f357891c343eb8812a26c840a8c794f662a67a4
bacebu4 <vasua14735@icloud.com>
2021-07-30 09:02:02 +0300

    feat: URLParser to Server

--- index.js
+++ index.js
@@ -1,22 +1,21 @@
 'use strict';
 
-const Config = require('./lib/config');
-const { Database } = require('./lib/sql/Database');
-const { URLParser } = require('./lib/server/URLParser');
+// const Config = require('./lib/config');
+// const { Database } = require('./lib/sql/Database');
 const { Server } = require('./lib/server/Server/Server');
 
 // @ts-check
 
 (async () => {
-  const config = await new Config('./src/config');
+  // const config = await new Config('./src/config');
 
-  const db = new Database(config.db);
+  // const db = new Database(config.db);
   // const res = await db.insert('users', { id: 4, name: 'vas', age: 12 });
   // console.log({ res });
   // db.delete('users', { name: '>vas', age: 3 });
   // db.update('users', { age: 3, name: 'vas3' }, { name: 'vas', age: '<3' });
-  const res = await db.findAll('users', { name: 'vasya' });
-  console.log(res[1]);
+  // const res = await db.findAll('users', { name: 'vasya' });
+  // console.log(res[1]);
 
   const routes = [
     {
@@ -45,21 +44,21 @@ const { Server } = require('./lib/server/Server/Server');
       handle() {
         console.log('hello4');
       },
-      method: 'GET',
+      method: 'POST',
     },
   ];
 
   // const rawIncomingUrl = 'id/124/update';
   // const rawIncomingUrl = 'users/get';
-  const requestUrl = 'users/get/23?apiKey=123&key=avx';
+  // const requestUrl = 'users/get/23?apiKey=123&key=avx';
 
-  const [error, { handler, params, query }] = URLParser.from({
-    requestUrl,
-    requestMethod: 'GET',
-    routes,
-  });
-  console.log({ handler, params, query });
-  console.log(error);
+  // const [error, { handler, params, query }] = URLParser.from({
+  //   requestUrl,
+  //   requestMethod: 'GET',
+  //   routes,
+  // });
+  // console.log({ handler, params, query });
+  // console.log(error);
 
-  new Server().start();
+  new Server(routes).start();
 })();
