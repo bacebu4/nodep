9b5a5675e43dbaff0d4bfaeb9e668e176c7d48d3
bacebu4 <vasua14735@icloud.com>
2021-08-02 15:49:02 +0300

    chore: upgraded eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,7 +5,7 @@ module.exports = {
     node: true,
     jest: true,
   },
-  extends: 'metarhia',
+  extends: ['standard'],
   globals: {
     Atomics: 'readonly',
     SharedArrayBuffer: 'readonly',
